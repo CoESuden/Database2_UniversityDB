2fc070de1d5936c40abbfd21c49049b948b254dc
Thanh Luong Giang <Thanh Luong Giang>
2020-06-07 21:13:52 +0200

    create new embedded database with the new schema

--- DB2_University/src/de/hft/main/Main.java
+++ DB2_University/src/de/hft/main/Main.java
@@ -1,16 +1,14 @@
 package de.hft.main;
 
 import de.hft.db.ConnectionHandler;
+import de.hft.db.DatabaseSchema;
 import de.hft.db.SQLStatementsUtil;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		SQLStatementsUtil.runDatabaseSchema();
-		SQLStatementsUtil.createTestDataForStudent();
-		SQLStatementsUtil.addStudent(99999, "TheOneWho", "GotAdded", "Something", 5.0);
+		// DatabaseSchema.runDBSchema();
 		SQLStatementsUtil.selectAllFromStudent();
-		SQLStatementsUtil.dropTableStudent();
 		ConnectionHandler.getInstance().closeConnection();
 	}
 }
